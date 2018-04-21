8af883c5d95d3ebcb8772c115a8d9740f80dbf19
Paul <i@jiangtaste.com>
2018-04-21 10:38:01 +0800

    update weixin signature

--- app/wexin/views.py
+++ app/wexin/views.py
@@ -1,8 +1,32 @@
 """ 微信视图模块 """
 from . import wx
+from flask import request
+import hashlib
 
 
-@wx.route('/wx')
-def wx():
+@wx.route('/wx', methods=['GET', 'POST'])
+def weixin():
     """ Wexin """
-    return "<h1>Hello World!</h1>"
+
+    if request.method == 'GET':
+        # Token, 同公众号服务器配置保持一只
+        token = "MII7DnXwxLOrzG0AMVJbhpQjRrECPHcs"
+        # 获取参数
+        data = request.args
+        signature = data.get('signature', '')
+        timestamp = data.get('timestamp', '')
+        nonce = data.get('nonce', '')
+        echostr = data.get('echostr', '')
+
+        # 进行字典排序
+        s = [token, timestamp, nonce]
+        s.sort()
+
+        # 拼接字符串
+        str = ''.join(s)
+
+        #比较
+        if hashlib.sha1(str).hexdigest() == signature:
+            return echostr
+        else:
+            return None
