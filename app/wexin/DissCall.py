12ad8c4de409e6d393c463bd1817e8b53a690fcd
Paul <i@jiangtaste.com>
2018-04-25 11:52:07 +0800

    优化号码正则：增加14X, 16X, 17X, 18X, 19X号段。支持输入+86开头的号码了

--- app/wexin/DissCall.py
+++ app/wexin/DissCall.py
@@ -84,13 +84,18 @@ def check_phone(phone):
     :param phone: 用户输入的电话号码
     :return phone_num: 纯数字的电话号码, phone
     """
-    phone_re = re.compile('^0\d{2,3}\d{7,8}$|^1[358]\d{9}$|^147\d{8}')
-
     # 去掉非数字的符号
     phone_num = re.sub("\D", "", phone)
 
+    # 座机+手机的正则匹配
+    phone_re = re.compile(
+        '^0\d{2,3}\d{7,8}$|^1[3456789]\d{9}$|^861[3456789]\d{9}$')
+
     if phone_re.match(phone_num):
-        # 验证通过则返回去除非空字符的纯数字号码
+        # 验证通过则返回去除非空字符的纯数字11位座机/手机号码
+        if len(phone_num) > 11:
+            # 例：+86 180-0000-0000
+            return phone_num[-11:-1]
         return phone_num
     else:
         return False
