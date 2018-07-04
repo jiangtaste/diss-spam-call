38bf93e3c53bac12b076507225cabcbb17901894
Paul <i@jiangtaste.com>
2018-07-04 10:42:44 +0800

    移除firebase相关业务

--- app/weixin/views.py
+++ app/weixin/views.py
@@ -4,104 +4,19 @@ import hashlib
 import time
 import json
 
-from flask import g, request, make_response, render_template, jsonify
-from app import redis_store, fire_store
+from flask import g, request, make_response, render_template
+from app import redis_store
 
 from .decorators import ratelimit, msg_parser
 from . import wx
 from .dispatch import dispatch
 
-# firestore refs
-bids_col = fire_store.collection('bids')  # 商户ID
-
-# 批量写入
-batch = fire_store.batch()
-
 
 @wx.route('/')
 @ratelimit(requests=20, window=60, by="ip")
 def index():
-    """ 获取可用商户ID """
-    # 从redis中获取bids
-    bids = redis_store.get('bids')
-
-    if bids:
-        # redis中存在bids
-        bids = json.loads(bids)
-    else:
-        # 不存在，初始化
-        bids = list(range(0, 9200))
-        redis_store.set('bids', json.dumps(bids))
-
-        # 30天过期
-        redis_store.expire('bids', 60 * 60 * 24 * 30)
-
-    return render_template('/index.html', bids=bids)
-
-
-@wx.route('/sync-bids', methods=['GET', 'POST'])
-@ratelimit(requests=20, window=60, by="ip")
-def sync_bid():
-    """ 同步商户ID """
-    if request.method == 'POST':
-        # POST
-        # 从redis中获取bids
-        bids = redis_store.get('bids')
-
-        if bids:
-            # redis中存在bids, 解析之
-            bids = json.loads(bids)
-
-        # 准备写入事物
-        times = len(bids) // 500
-        if times > 1:
-            # 大于等于500
-            for x in range(times):
-                # 分片
-                for bid in bids[(x * 500):(x * 500 + 500)]:
-                    batch.set(bids_col.document(str(bid)), {'name': bid})
-
-                # 批量写入fierestore
-                batch.commit()
-
-            # 不能被500整除的部分
-            for bid in bids[(times * 500):]:
-                batch.set(bids_col.document(str(bid)), {'name': bid})
-
-            # 批量写入fierestore
-            batch.commit()
-        else:
-            # 小于500
-            for bid in bids:
-                batch.set(bids_col.document(str(bid)), {'name': bid})
-
-            # 批量写入fierestore
-            batch.commit()
-
-        # 从firestore读取
-        bid_docs = bids_col.get()
-
-        bids = []
-        for doc in bid_docs:
-            bids.append({'id': doc.id, 'data': doc.to_dict()})
-
-        # 生成响应
-        response = jsonify(bids)
-        response.status_code = 201
-        return response
-    else:
-        # GET
-        # 从firestore中获取bids
-        bid_docs = bids_col.get()
-
-        bids = []
-        for doc in bid_docs:
-            bids.append({'id': doc.id, 'data': doc.to_dict()})
-
-        # 生成响应
-        response = jsonify(bids)
-        response.status_code = 200
-        return response
+    """ 什么也不做 """
+    return 'just a joke tools.'
 
 
 @wx.route('/wx', methods=['GET', 'POST'])
